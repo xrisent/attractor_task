7c24a208e593d756c0922caf433b058d92c992d5
xrisent <ormonov31261@gmail.com>
2025-04-24 19:56:43 +0600

    route fix

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: "/",
+        path: "/profile",
         element: <ProfilePage />,
       },
     ],
